41ff46d87a543a7b0c7824c8b758daebdb95810d
Djangojie <yekejieandhan@163.com>
2022-10-28 10:12:36 +0800

    修正

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -39,19 +39,6 @@ public class Feeder {
     public void setName(String name) {
         this.name = name;
     }
-}
-
-
-public class Test {
-    public static void main(String[] args) {
-        Dog pig = new Pig();
-        Cat cat = new Cat();
-        Feeder m1 = new Feeder();
-        Feeder m2 = new Feeder();
-        Feeder m3 = new Feeder();
-        feeder.feed(pig);
-        feeder.feed(cat);
-    }
-}
+}}
 
 
